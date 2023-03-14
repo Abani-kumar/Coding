b08b6b5a1d75fe7e96d0ab6262333d8c1abc1fd3
Abani-kumar <125666093+Abani-kumar@users.noreply.github.com>
2023-03-14 19:59:24 +0530

    Create nearby Cars.java

---
+++ nearby Cars.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Heap {
+    static class point implements Comparable<point> {
+        int x;
+        int y;
+        int distSq;
+        int idx;
+        public point(int x,int y,int distSq,int idx){
+            this.x=x;
+            this.y=y;
+            this.distSq=distSq;
+            this.idx=idx;
+        }
+        @Override
+        public int compareTo(point p2){
+            return this.distSq-p2.distSq;
+        }
+    }
+    public static void main(String[] args) {
+        int arr[][]={{3,3},
+                {5,-1},
+                {-2,4}
+        };
+        int k=2;
+        PriorityQueue<point>pq=new PriorityQueue<>();
+        for(int i=0;i<arr.length;i++){
+            int distSq=(arr[i][0]*arr[i][0])+(arr[i][1]*arr[i][1]);
+            pq.add(new point(arr[i][0],arr[i][1],distSq,i));
+        }
+        for(int i=0;i<k;i++){
+            System.out.println("C"+pq.remove().idx);
+        }
+    }
+}
